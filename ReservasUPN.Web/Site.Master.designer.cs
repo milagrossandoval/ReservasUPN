3fd409024aa699f9517aa7fa2946f8d594b7d6d0
Milagros Sandoval <mily.sandoval15@gmail.com>
2017-07-29 14:14:23 -0500

    Subiendo nuevamente todos los archivos.

--- ReservasUPN.Web/Site.Master.designer.cs
+++ ReservasUPN.Web/Site.Master.designer.cs
@@ -9,7 +9,7 @@
 
 
 
-public partial class  {
+public partial class Site {
     
     /// <summary>
     /// head control.
@@ -30,13 +30,22 @@ public partial class  {
     protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     
     /// <summary>
-    /// sm1 control.
+    /// RadScriptManager1 control.
     /// </summary>
     /// <remarks>
     /// Auto-generated field.
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
-    protected global::System.Web.UI.ScriptManager sm1;
+    protected global::Telerik.Web.UI.RadScriptManager RadScriptManager1;
+    
+    /// <summary>
+    /// RadSkinManager1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadSkinManager RadSkinManager1;
     
     /// <summary>
     /// ContentPlaceHolder1 control.
