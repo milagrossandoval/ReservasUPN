0c93db13ff803e5d868a22e223d68387aeec3f87
elgrandikas <diego17castaneda@gmail.com>
2017-08-17 19:48:33 -0500

    jqueri ui

---
+++ ReservasUPN.Web/Secure/RecursosTipo.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ReservasUPN.Web.App_Code;
+
+namespace ReservasUPN.Web.Secure
+{
+    public partial class RecursosTipo : PageAdapter
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
